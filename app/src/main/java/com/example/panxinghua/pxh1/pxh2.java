d431b99dc175b1a206776694ab0249d34f68ef00
panxinghua <177708120@qq.com>
2016-05-07 07:46:40 +0800

    add pxh2

---
+++ app/src/main/java/com/example/panxinghua/pxh1/pxh2.java
@@ -0,0 +1,18 @@
+package com.example.panxinghua.pxh1;
+
+import android.os.Bundle;
+
+/**
+ * Created by panxinghua on 16/5/7.
+ */
+public class pxh2 extends pxh1{
+
+    @Override
+    protected  void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another_aty);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(E.id.faab);
+
+    }
+}
